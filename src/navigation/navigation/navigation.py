2ff4ba044543c58035a50ddedfa32c3b4c2e11f3
andr00d <roel.vries@outlook.com>
2024-06-06 14:33:36 +0200

    quick start on nav node

--- src/navigation/navigation/navigation.py
+++ src/navigation/navigation/navigation.py
@@ -16,14 +16,43 @@ class Navigation(Node):
         super().__init__("Navigation")
 
         self.subscriber = self.create_subscription(Objects, "objects", self._objects_callback, 10)
-        self.pub_surveillance_ = self.create_publisher(Objects, "state_surveillance", 10)
+        self.pub_surveillance = self.create_publisher(Objects, "state_surveillance", 10)
         self.pub_guard = self.create_publisher(Objects, "state_guard", 10)
         self.pub_home = self.create_publisher(Objects, "state_home", 10)
 
-    def _objects_callback(self, msg):
-        # Do something with the objects, then send objects to correct node
+        self.curr_state = None
 
-        self.pub_surveillance_.publish(msg)
+    def _objects_callback(self, msgs):
+        self.pub_surveillance.publish(msgs)
+        return
+
+        gestures = ["gesture_1", "gesture_2"]
+        hands = [m for m in msgs.objects if m.type in gestures]
+        biggest_hand = max(msgs.objects, key=lambda m: m.width * m.height)
+
+        if(len(gestures == 0) and self.curr_state != None):
+            self.curr_state.publish(msgs)
+
+        if("stop" in [m.type for m in hands.type] and self.curr_state != None):
+            object_msg = Object()
+            object_msg.type = "clear"
+
+            objects_msg = Objects()
+            objects_msg.objects = [object_msg]
+            
+            self.curr_state.publish(objects_msg)
+            self.curr_state = None
+
+        else:
+            if biggest_hand.type == "surveillance":
+                self.curr_state = self.pub_surveillance
+            if biggest_hand.type == "guard":
+                self.curr_state = self.pub_guard
+            if biggest_hand.type == "pub_home":
+                self.curr_state = self.pub_home
+
+
+        self.pub_surveillance.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
